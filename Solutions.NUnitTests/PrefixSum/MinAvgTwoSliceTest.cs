2960329a5877411b49d6256c9da76f67853ed3ce
Luiz Filho <Lfilho@gap.com>
2019-10-13 18:30:07 -0300

    Add new MinAvgTwoSlice

---
+++ Solutions.NUnitTests/PrefixSum/MinAvgTwoSliceTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Solutions.NetCore.PrefixSum;
+
+namespace Solutions.NUnitTests.PrefixSum
+{
+    [TestFixture]
+    public class MinAvgTwoSliceTest
+    {
+        [TestCase(new int[] { 4, 2, 2, 5, 1, 5, 8 }, ExpectedResult = 1)]
+        public int MinAvgTwoSliceTestBasic(int [] A)
+        {
+            return MinAvgTwoSlice.Solution(A);
+        }
+    }
+}
---
+++ Solutions.NetCore/PrefixSum/MinAvgTwoSlice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions.NetCore.PrefixSum
+{
+    public static class MinAvgTwoSlice
+    {
+        public static int Solution(int[] A)
+        {
+            int[] prefixSum = PrefixSum(A);
+            int prefixSumSize = prefixSum.Length;
+            int result = 0;
+            decimal minValue = decimal.MaxValue;
+            decimal avg1;
+            decimal avg2;
+            for (int i = 0; i < prefixSumSize - 1; i++)
+            {
+                avg1 = (prefixSum[i + 1] - prefixSum[i] + A[i]) / 2M;
+                if(avg1 < minValue )
+                {
+                    result = i;
+                    minValue = avg1;
+                }
+
+                if(i + 2 < prefixSumSize)
+                {
+                    avg2 = (prefixSum[i + 2] - prefixSum[i] + A[i]) / 3M;
+                    if(avg2 < minValue)
+                    {
+                        result = i;
+                        minValue = avg2;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static int[] PrefixSum(int[] A)
+        {
+            var prefixSum = new int[A.Length];
+            prefixSum[0] = A[0];
+            for (int i = 1; i < A.Length; i++)
+            {
+                prefixSum[i] = prefixSum[i - 1] + A[i];
+            }
+
+            return prefixSum;
+        }
+    }
+}
